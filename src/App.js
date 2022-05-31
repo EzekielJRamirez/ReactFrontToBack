c5ba48fd181e0b2be196771bf855f745ee6c54c6
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2022-05-30 21:09:57 -0700

    Read the comments!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <h1>Hello from the app component</h1>
 }
-
-export default App;
+// I needed to add a blank line after the export to
+// get the code to work!!!
+export default App
